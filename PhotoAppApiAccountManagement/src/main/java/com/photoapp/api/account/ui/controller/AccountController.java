ae49190ee685c1719b331b062619c6cc1d7a2e4b
kumarvipin1983 <kumar.ricky@gmail.com>
2022-01-16 22:27:34 +0530

    update path in controller

--- PhotoAppApiAccountManagement/src/main/java/com/photoapp/api/account/ui/controller/AccountController.java
+++ PhotoAppApiAccountManagement/src/main/java/com/photoapp/api/account/ui/controller/AccountController.java
@@ -1,15 +1,15 @@
-package com.photoapp.api.users.ui.controller;
+package com.photoapp.api.account.ui.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/actuator")
-public class UserController {
+@RequestMapping("/actuator")
+public class AccountController {
 
-	@GetMapping(path = "info")
+	@GetMapping("info")
 	public String status() {
-		return "Working";
+		return "Working ...";
 	}
 }
