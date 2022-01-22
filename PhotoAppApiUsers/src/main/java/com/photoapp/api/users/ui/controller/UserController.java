1064fa67d39a2932be1f66276686b66e55fa451e
kumarvipin1983 <kumar.ricky@gmail.com>
2022-01-23 01:32:51 +0530

    Fix applied

--- PhotoAppApiUsers/src/main/java/com/photoapp/api/users/ui/controller/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/ui/controller/UserController.java
@@ -6,6 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,12 +34,12 @@ public class UserController {
 	}
 
 	@PostMapping
-	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+	public ResponseEntity<CreateUserRequestModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
 		userService.createUser(userDto);
-		return "Create user method is called.";
+		return new ResponseEntity<CreateUserRequestModel>(userDetails, HttpStatus.CREATED);
 	}
 }
