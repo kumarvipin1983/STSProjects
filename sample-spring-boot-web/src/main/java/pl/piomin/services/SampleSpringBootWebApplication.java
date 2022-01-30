62a7e08f9bd0048bfbacf07bce2f485fdbc0702a
kumarvipin1983 <kumar.ricky@gmail.com>
2022-01-30 20:26:44 +0530

    more learning

---
+++ sample-spring-boot-web/src/main/java/pl/piomin/services/SampleSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package pl.piomin.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootWebApplication.class, args);
+	}
+
+}
