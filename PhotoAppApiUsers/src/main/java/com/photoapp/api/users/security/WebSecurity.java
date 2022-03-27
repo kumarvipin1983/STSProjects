479d8d31218f4a9c3e02cfa9ecb8988dfc21455e
kumarvipin1983 <kumar.ricky@gmail.com>
2022-03-27 23:16:38 +0530

    Add Config Server API and Settings

--- PhotoAppApiUsers/src/main/java/com/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/security/WebSecurity.java
@@ -20,12 +20,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		System.out.println(env.getProperty("gateway.ip"));
+		// System.out.println(env.getProperty("gateway.ip"));
 		// http.authorizeRequests().antMatchers("/login").permitAll();
 		// http.authorizeRequests().antMatchers("/users/**").hasIpAddress(env.getProperty("gateway.ip"));
 		// http.authorizeRequests().antMatchers("/**").hasIpAddress("192.168.1.9").anyRequest().authenticated();
 		http.authorizeHttpRequests().antMatchers("/**").permitAll();
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
+		super.configure(http);
 	}
 }
