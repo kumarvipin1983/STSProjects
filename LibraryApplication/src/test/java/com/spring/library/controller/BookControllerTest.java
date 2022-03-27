938ae8d7bf9d2d169cecd310714b1985f31fd3c9
kumarvipin1983 <kumar.ricky@gmail.com>
2022-03-27 15:40:09 +0530

    Fix Eureka docker setup

--- LibraryApplication/src/test/java/com/spring/library/controller/BookControllerTest.java
+++ LibraryApplication/src/test/java/com/spring/library/controller/BookControllerTest.java
@@ -6,10 +6,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,8 +20,8 @@ import com.spring.library.interfaces.BookService;
 import com.spring.library.model.Book;
 
 @RunWith(SpringRunner.class)
-@AutoConfigureMockMvc
-@WebMvcTest(BookController.class)
+//@AutoConfigureMockMvc
+@WebMvcTest(controllers = BookController.class)
 public class BookControllerTest {
 
 	@Autowired
@@ -29,6 +30,7 @@ public class BookControllerTest {
 	@MockBean
 	private BookService bookService;
 
+	@Test
 	public void shouldReturnListOfBooks() throws Exception {
 		List<Book> booklist = new ArrayList<>();
 		booklist.add(new Book("123", "Spring 5 Recipes", "Marten Deinum", "Josh Long"));
@@ -36,5 +38,11 @@ public class BookControllerTest {
 		when(bookService.findAll()).thenReturn(booklist);
 
 		this.mockMvc.perform(get("/books")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void shouldReturn404WhenBookNotFound() throws Exception {
+		when(bookService.find("4527")).thenReturn(Optional.empty());
+		this.mockMvc.perform(get("/books/4527")).andExpect(status().isNotFound());
 	}
 }
--- PhotoAppApiUsers/src/main/java/com/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,12 +2,12 @@ package com.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
